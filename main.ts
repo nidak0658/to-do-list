c017fd4ff8b7e78f564fe0c52f31815b52e3b775
nidak0658 <165899145+nidak0658@users.noreply.github.com>
2024-04-05 20:07:01 +0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 #! /usr/bin/env node
 
-import inquirer from "inquirer";
+// Part 01
 
- let todos = [];
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+ /* let todos = [];
  let condition = true;
 
 while(condition)
@@ -17,13 +20,13 @@ while(condition)
                 {
                     name:'addMore',
                     type:'confirm',
-                    message:"Do you want to add more ?",
+                    message:"Do you want to add more?",
                     default:"false"
                 }
             ]
         );
         todos.push(addTask.todo);
-        condition = addTask.addMore
+        condition = addTask.addMore;
         console.log(todos)
     }
     
@@ -42,12 +45,104 @@ while(location)
                 {
                     name:'noMore',
                     type:'confirm',
-                    message:"Do you want to no more place ?",
+                    message:"Do you want to no more place?",
                     default:"false"
                 }
             ]
         );
         todays.push(deleteOption.today);
-        location = deleteOption.noMore
+        location = deleteOption.noMore;
         console.log(todays)
-    }
+    } */
+
+// Part 02
+
+ /* let todos = []; // Shopper []
+ let condition = true;
+
+while(condition)
+    {
+        let todoQuestions = await inquirer.prompt(
+            [
+                {
+                    name:'firstQuestion',
+                    type:'input',
+                    message:"What would you like to add in you todos?"
+                },
+                {
+                    name:'secondQuestion',
+                    type:'confirm', // When type is confirm answer is in yes or no
+                    message:"Would you like to add more in your todos?",
+                    default:"true"
+                }
+            ]
+        );
+        todos.push(todoQuestions.firstQuestion);
+        // The loop is running on the based of this variable condition 
+        condition = todoQuestions.secondQuestion;
+        console.log(todos)
+    } */
+
+// Home Work
+// Read, Update, Add, Delete, Empty
+
+// Part 03
+
+ let Task1: string[] = ["Sadaf", "Hadaf", "Huda", "Fiza"];
+ let condition = true;
+async function createToday(Task1: string[]) {
+    do {
+        let ans = await inquirer.prompt({
+            name: "select",
+            type: "list",
+            message: " select an operation",
+            choices: ["Add", "update", "view", "delete"],
+        });
+        if (ans.select == "Add") {
+            let addToday = await inquirer.prompt({
+                type: "input",
+                message: "Add items in the list",
+                name: "today",
+            });
+            Task1.push(addToday.today);
+            Task1.forEach((today) => console.log(today));
+            // console.log(Task1);
+        }
+        if (ans.select == "update") {
+            let uupdateToday = await inquirer.prompt({
+                type: "list",
+                message: "update items in the list",
+                name: "today",
+                choices: Task1.map((item) => item),
+            });
+            let addToday = await inquirer.prompt({
+                type: "input",
+                message: "Add items in the list",
+                name: "today",
+            });
+            let newToday = Task1.filter((val) => val != uupdateToday.today);
+            Task1 = [...newToday, addToday.today];
+            console.log(Task1);
+        }
+        if (ans.select == "view") {
+            console.log("\t***TO DO LIST***");
+            console.log(Task1);
+            console.log("\t**************");
+        }
+        if (ans.select == "delete") {
+            let deleteToday = await inquirer.prompt({
+                type: "list",
+                message: "update items in the list",
+                name: "today",
+                choices: Task1.map((item) => item),
+            });
+            let newToday = Task1.filter((val) => val != deleteToday.today);
+            Task1 = [...newToday];
+            console.log(chalk.bgBlueBright(Task1));
+        }
+    } while (true);
+}
+createToday(Task1);
+if (Task1.length > 0) {
+    console.log(`List of TODO's ${Task1}`);
+}
